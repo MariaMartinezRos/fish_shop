ff750ede5e3c18a6e5aeb14e95bbc4f1a0c522e7
MariaMartinezRos <cibernuggets@gmail.com>
2024-11-21 17:58:19 +0100

    sales, sale_details, expenses and transactions table

---
+++ database/migrations/2024_11_21_163625_create_sale_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sale_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sale_id')->index();
+            $table->unsignedBigInteger('product_id')->index();
+            $table->decimal('quantity_kg', 10, 2);
+            $table->decimal('price_per_kg', 10, 2);
+            $table->decimal('subtotal', 10, 2);
+
+            $table->foreign('sale_id')->references('id')->on('sales')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sale_details');
+    }
+};
---
+++ database/migrations/2024_11_21_163856_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('tpv');
+            $table->string('serial_number');
+            $table->string('terminal_number');
+            $table->string('operation');
+            $table->decimal('amount', 10, 2);
+            $table->string('card_number');
+            $table->dateTime('date_time');
+            $table->string('transaction_number');
+            $table->unsignedBigInteger('sale_id')->index();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+
+            $table->foreign('sale_id')->references('id')->on('sales')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
