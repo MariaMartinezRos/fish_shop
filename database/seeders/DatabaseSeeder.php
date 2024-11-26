479f0fd764cd0f2b3cdf953d02b715fc5f760798
MariaMartinezRos <cibernuggets@gmail.com>
2024-11-26 18:51:29 +0100

    add more available products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -52,7 +52,18 @@ class DatabaseSeeder extends Seeder
                 ['name' => 'camarón', 'category_id' => $categories[3]->id, 'price_per_kg' => '25.00', 'stock_kg' => '8.00', 'description' => 'Camarón fresco'],
                 ['name' => 'pargo', 'category_id' => $categories[0]->id, 'price_per_kg' => '18.50', 'stock_kg' => '5.00', 'description' => 'Pargo fresco'],
                 ['name' => 'tilapia', 'category_id' => $categories[0]->id, 'price_per_kg' => '6.50', 'stock_kg' => '20.00', 'description' => 'Tilapia de cultivo'],
-                ['name' => 'sardinas', 'category_id' => $categories[1]->id, 'price_per_kg' => '4.50', 'stock_kg' => '10.00', 'description' => 'Sardinas congeladas']
+                ['name' => 'sardinas', 'category_id' => $categories[1]->id, 'price_per_kg' => '4.50', 'stock_kg' => '10.00', 'description' => 'Sardinas congeladas'],
+                ['name' => 'preparado', 'category_id' => $categories[1]->id, 'price_per_kg' => '10.00', 'stock_kg' => '8.00', 'description' => 'Preaparado para paella'],
+                ['name' => 'pulpo', 'category_id' => $categories[2]->id, 'price_per_kg' => '22.00', 'stock_kg' => '4.00', 'description' => 'Pulpo fresco'],
+                ['name' => 'sepia', 'category_id' => $categories[2]->id, 'price_per_kg' => '15.00', 'stock_kg' => '6.00', 'description' => 'Sepia fresca'],
+                ['name' => 'gambas', 'category_id' => $categories[3]->id, 'price_per_kg' => '20.00', 'stock_kg' => '5.00', 'description' => 'Gambas frescas'],
+                ['name' => 'cigalas', 'category_id' => $categories[3]->id, 'price_per_kg' => '30.00', 'stock_kg' => '3.00', 'description' => 'Cigalas frescas'],
+                ['name' => 'bogavante', 'category_id' => $categories[3]->id, 'price_per_kg' => '50.00', 'stock_kg' => '1.00', 'description' => 'Bogavante fresco'],
+                ['name' => 'vieiras', 'category_id' => $categories[3]->id, 'price_per_kg' => '35.00', 'stock_kg' => '2.00', 'description' => 'Vieiras frescas'],
+                ['name' => 'cangrejo', 'category_id' => $categories[3]->id, 'price_per_kg' => '25.00', 'stock_kg' => '4.00', 'description' => 'Cangrejo fresco'],
+                ['name' => 'carabineros', 'category_id' => $categories[3]->id, 'price_per_kg' => '40.00', 'stock_kg' => '1.00', 'description' => 'Carabineros frescos'],
+                ['name' => 'salmonete', 'category_id' => $categories[0]->id, 'price_per_kg' => '14.00', 'stock_kg' => '7.00', 'description' => 'Salmonete frescos']
+
 
         ];
 
