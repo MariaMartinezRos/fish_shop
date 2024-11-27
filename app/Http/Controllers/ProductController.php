71beb5223ccb714b8699b36a6c94ee63d3662dbf
MariaMartinezRos <cibernuggets@gmail.com>
2024-11-27 19:22:59 +0100

    paginate stock

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,7 +10,9 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = DB::table('products')->get();
+//        $products = DB::table('products')->get();
+
+        $products = Product::paginate(10); // 10 productos por página
 
         if ($products->isEmpty()) {
             return view('stock', ['products' => []]);
@@ -17,4 +20,9 @@ class ProductController extends Controller
 
         return view('stock', compact('products'));
     }
+//    public function showStock()
+//    {
+//        $products = Product::paginate(10); // 10 productos por página
+//        return view('stock', compact('products'));
+//    }
 }
--- resources/views/stock.blade.php
+++ resources/views/stock.blade.php
@@ -9,9 +9,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("The stock section") }}
-                </div>
-                <div class="p-6 text-gray-900 dark:text-gray-100">
 
                     @if(isset($products) && count($products) > 0)
                         @php
@@ -28,55 +25,10 @@
                             }
                         @endphp
                         <x-products-list :products="$productList" />
+                        {{ $products->links() }}
                     @else
                         <p>{{ __("No products found.") }}</p>
                     @endif
-
-{{--                    @if(isset($products) && count($products) > 0)--}}
-{{--                        @foreach($products as $product)--}}
-{{--                            @php--}}
-{{--                                $products = [--}}
-{{--                                        'image' =>  asset('images/'.$product->name.'.png'),--}}
-{{--                                        'alt' => $product->name,--}}
-{{--                                        'name' => $product->name,--}}
-{{--                                        'description' => $product->description,--}}
-{{--                                        'price' => $product->price_per_kg--}}
-{{--                                    ];--}}
-{{--                            @endphp--}}
-{{--                            <x-products-list :products="$products" />--}}
-{{--                        @endforeach--}}
-{{--                        @else--}}
-{{--                            <p>{{ __("No products found.") }}</p>--}}
-{{--                        @endif--}}
-    {{--                    @if(isset($products) && count($products) > 0)--}}
-    {{--                        @foreach($products as $product)--}}
-    {{--                            <h2>{{ __("Name: ") }}{{ $product->name }}</h2><br/>--}}
-    {{--                            <h2>{{ __("Price (€/Kg): ") }}{{ $product->price_per_kg }}</h2><br/>--}}
-    {{--                            <h2>{{ __("Description: ") }}{{ $product->description }}</h2><br/><br/>--}}
-    {{--                        @endforeach--}}
-    {{--                    @else--}}
-    {{--                        <p>{{ __("No products found.") }}</p>--}}
-    {{--                    @endif--}}
-{{--                            @php--}}
-{{--                                $products = [--}}
-{{--                                    [--}}
-{{--                                        'image' =>  asset('images/sardinas.png'),--}}
-{{--                                        'alt' => 'sardinas',--}}
-{{--                                        'name' => 'sardinas',--}}
-{{--                                        'description' => 'sardinas',--}}
-{{--                                        'price' => '$35'--}}
-{{--                                    ],--}}
-{{--                                    [--}}
-{{--                                        'image' =>  asset('images/salmón.png'),--}}
-{{--                                        'alt' => 'salmón',--}}
-{{--                                        'name' => 'salmón',--}}
-{{--                                        'description' => 'salmón',--}}
-{{--                                        'price' => '$35'--}}
-{{--                                    ]--}}
-{{--                                ];--}}
-{{--                        @endphp--}}
-
-
                 </div>
             </div>
         </div>
