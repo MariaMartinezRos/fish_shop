03bd778a6142da61719d88a8b2d3b07822aaaf64
MariaMartinezRos <cibernuggets@gmail.com>
2025-01-24 16:24:17 +0100

    arreglar SalesPerHourChart para sqlite

--- app/Charts/SalesPerHourChart.php
+++ app/Charts/SalesPerHourChart.php
@@ -18,13 +18,25 @@ class SalesPerHourChart extends Chart
     }
     public function build(): void
     {
-        // crear una variable con la consulta a la base de datos
-        $salesData = DB::table('transactions')
-            //selecciono hora por hora y sumo el total de las ventas
-            ->select(DB::raw('HOUR(date_time) as hour'), DB::raw('SUM(amount) as total'))
-            ->groupBy('hour')
-            ->orderBy('hour')
-            ->get();
+        /**
+         * SQLite no tiene la función HOUR() para extraer la hora de un campo datetime
+         * por lo que se usa strftime('%H', date_time) para extraer la hora
+         */
+        $connection = DB::connection()->getDriverName();
+
+        if ($connection === 'sqlite') {
+            $salesData = DB::table('transactions')
+                ->select(DB::raw('strftime(\'%H\', date_time) as hour'), DB::raw('SUM(amount) as total'))
+                ->groupBy('hour')
+                ->orderBy('hour')
+                ->get();
+        } else {
+            $salesData = DB::table('transactions')
+                ->select(DB::raw('HOUR(date_time) as hour'), DB::raw('SUM(amount) as total'))
+                ->groupBy('hour')
+                ->orderBy('hour')
+                ->get();
+        }
 
         $hours = $salesData->pluck( __('hour') )->toArray();
         $totals = $salesData->pluck( __('total') )->toArray();
