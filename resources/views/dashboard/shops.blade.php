5404a7002d3cc515083d04dc0e75157dbe14dfc6
MariaMartinezRos <cibernuggets@gmail.com>
2025-02-10 20:42:00 +0100

    shops view, tests and images

--- resources/views/dashboard/shops.blade.php
+++ resources/views/dashboard/shops.blade.php
@@ -1,2 +1,32 @@
 @include('partials.nav-bar-client')
+
+<div class="flex justify-center items-center min-h-screen p-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div class="bg-blue-200 p-12 rounded-2xl shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-2xl">
+            <img
+                src="{{ asset('images/fishes/image05.jpg') }}"
+                alt="{{ __('Benito\'s Fish Markets') }} TIENDA 1"
+                class="w-full h-80 object-cover rounded-xl"
+            />
+            <div class="mt-8 text-center">
+                <h2 class="text-3xl font-bold text-blue-800">{{ __('Benito\'s Fish Markets') }} ALHAMA</h2>
+                <p class="text-blue-600">123 Seaside Ave, Miami, FL</p>
+            </div>
+        </div>
+        <div class="bg-blue-200 p-12 rounded-2xl shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-2xl">
+            <img
+                src="{{ asset('images/fishes/image07.jpg') }}"
+                alt="{{ __('Benito\'s Fish Markets' ) }} TIENDA 2"
+                class="w-full h-80 object-cover rounded-xl"
+            />
+            <div class="mt-8 text-center">
+                <h2 class="text-3xl font-bold text-blue-800">{{ __('Benito\'s Fish Markets') }} LIBRILLA</h2>
+                <p class="text-blue-600">456 Harbor St, San Diego, CA</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 @include('partials.footer')
+
--- tests/Feature/Pages/customer/ShopsTest.php
+++ tests/Feature/Pages/customer/ShopsTest.php
@@ -5,3 +5,19 @@ it('returns a successful response for shops page', function () {
 
     $response->assertStatus(200);
 });
+
+it('shows the image and name of the shops', function () {
+    $response = $this->get('shops');
+
+    $response->assertSee('Pescaderías Benito TIENDA 1');
+    $response->assertSee('Pescaderías Benito TIENDA 2');
+    $response->assertSee('image05.jpg');
+    $response->assertSee('image07.jpg');
+});
+
+it('shows the direction of the shops', function () {
+    $response = $this->get('shops');
+
+    $response->assertSee('123 Seaside Ave, Miami, FL');
+    $response->assertSee('456 Harbor St, San Diego, CA');
+});
