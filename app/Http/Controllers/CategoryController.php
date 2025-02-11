c98bb9037ece56a9eecc1e2eaaa0787a1b2839ed
MariaMartinezRos <cibernuggets@gmail.com>
2025-02-11 18:57:35 +0100

    desarrollar politicas de acceso (policies) category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
+    use AuthorizesRequests;
+
     /**
      * Muestra la lista de categorías.
      */
     public function index()
     {
+        $this->authorize('view', Category::class);
+
+        // Consulta en la base de datos para obtener todas las categorías
         $categories = DB::table('categories')->get();
 
         if ($categories->isEmpty()) {
